5c2c6e023b4b01ca7de5bc6f98798d679821900c
Anna Kilikova <103356373+klikli19@users.noreply.github.com>
2024-10-18 11:23:53 +0300

    install commit

---
+++ src/main/java/ru/job4j/lesson_git/LessonGitApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.lesson_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/job4j/lesson_git/LessonGitApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.lesson_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
